13900533d4f43dd5086e147058fdc329be6031cc
Samuel Almeida <barbosasamuel24@gmail.com>
2024-05-06 19:31:58 -0300

    fix translateToPigLatin

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,37 @@
 public class Main {
     public static String translateToPigLatin(String word) {
-
+        boolean isCapitalized = Character.isUpperCase(word.charAt(0));
         word = word.toLowerCase();
-
         int firstVowelIndex = -1;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i'
-                    || ch == 'o' || ch == 'u' || ch == 'y'
-            ) {
+            if (ch == 'a' || ch == 'e' || ch == 'i'
+             || ch == 'o' || ch == 'u' || ch == 'y'
+             ) {
                 firstVowelIndex = i;
                 break;
             }
         }
 
         if ( firstVowelIndex == -1) {
-            return word + "yay";
+            word = word + "yay";
+        } else {
+            String prefix = word.substring(0, firstVowelIndex);
+            String stem = word.substring(firstVowelIndex);
+            word = stem + prefix + "yay";
         }
 
-        String prefix = word.substring(0, firstVowelIndex);
-        String stem = word.substring(firstVowelIndex);
+        if (isCapitalized) {
+            word = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+        }
 
-        return stem + prefix + "yay";
-
+        return word;
     }
 
     public static String translateSentenceToPigLatin(String sentence) {
         StringBuilder translatedSentence = new StringBuilder();
 
-        String [] words = sentence.split("\\s+ ");
+        String [] words = sentence.split("\\s+");
 
         for (String word : words) {
             String punctuation = "";
@@ -52,7 +55,7 @@ public class Main {
             }
 
             translatedSentence.append(translatedWord).append(punctuation).append(" ");
-
+            
         }
         return translatedSentence.toString().trim();
     }
@@ -60,15 +63,22 @@ public class Main {
     public static void main(String[] args) {
         String input1 = "Stop";
         String output1 = translateToPigLatin(input1);
-        System.out.println("input: " + input1 + "Output: " + output1);
+        System.out.println("Input: " + input1 + " Output: " + output1);
 
-        String input2 = "Away";
-        String output2 = translateToPigLatin(input2);
-        System.out.println("input: " + input2 + "Output: " + output2);
+        String input2 = "No littering";
+        String output2 = translateSentenceToPigLatin(input2);
+        System.out.println("Input: " + input2 + " Output: " + output2);
 
-        String input3 = "No persons under 14 admitted\n";
+        String input3 = "No shirts, no shoes, no service";
         String output3 = translateSentenceToPigLatin(input3);
-        System.out.println("input: " + input3 + "Output: " + output3);
+        System.out.println("Input: " + input3 + " Output: " + output3);
 
+        String input4 = "No persons under 14 admitted";
+        String output4 = translateSentenceToPigLatin(input4);
+        System.out.println("Input: " + input4 + " Output: " + output4);
+
+        String input5 = "Hey buddy, get away from my car!";
+        String output5 = translateSentenceToPigLatin(input5);
+        System.out.println("Input: " + input5 + " Output: " + output5);
     }
 }
